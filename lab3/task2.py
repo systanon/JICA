c413da0411f45baabd81749647f3de9915d0955f
Serhii <tustanovskiis@gmail.com>
2025-07-27 10:11:02 +0200

    feat: done task 2

---
+++ lab3/task2.py
@@ -0,0 +1,14 @@
+import re
+
+
+def formatter(text):
+    formatted_text = re.sub(
+        r"python", "PYTHON", text.strip().lower(), flags=re.IGNORECASE
+    )
+    return formatted_text
+
+
+res = formatter(input("Input text: "))
+
+
+print(res)
