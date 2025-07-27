ed6a31f94481847c7040b7c2a01d185a099f1bc8
Serhii <tustanovskiis@gmail.com>
2025-07-27 21:39:17 +0200

    fix: task5

--- lab3/task5.py
+++ lab3/task5.py
@@ -4,7 +4,7 @@ from utils.utils import prettifyDict
 def rectangleCalculator(a, b):
     a = float(a)
     b = float(b)
-    return {"area": a * b, "circuit": 2 * (a * b)}
+    return {"area": a * b, "circuit": 2 * (a + b)}
 
 
 prettifyDict(rectangleCalculator(input("Enter side a: "), input("Enter side b: ")))
