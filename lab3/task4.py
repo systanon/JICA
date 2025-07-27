07bbe611a1a32b3b92bc2baa51f2c183e88ffe23
Serhii <tustanovskiis@gmail.com>
2025-07-27 21:59:32 +0200

    fix: used allocation task 4

--- lab3/task4.py
+++ lab3/task4.py
@@ -1,8 +1,8 @@
 def reverse_words(text):
     parts = text.strip().split()
-    result = []
-    for word in parts:
-        result.append(word[::-1])
+    result = [" "] * len(parts)
+    for index, word in enumerate(parts):
+        result[index] = word[::-1]
     return " ".join(result)
 
 
